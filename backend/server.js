cc4f4f960ec720e7fa27a52d5482b49c43f0d4fe
Shivvani <shivvani.m.2024.it@rajalakshmi.edu.in>
2026-04-05 12:22:11 +0530

    fix db

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-// server.js
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// ✅ Connect to MongoDB Atlas
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/stocksDB");
-
+// Routes
 app.use("/api", require("./routes/stockRoutes"));
 
-app.listen(5000, () => console.log("Backend running"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
